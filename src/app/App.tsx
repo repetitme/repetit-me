6b1067a30e9d0cb53650a4d9f286ab3eafbdc4e9
Maxim Beketov <cleanerkerri@gmail.com>
2025-02-22 00:17:59 +0500

    chore: delete all test component, change readme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,35 +1,5 @@
-import { useState } from 'react';
-import classNames from 'classnames';
-import styles from '../assets/styles/app.module.scss';
-import logo from '../assets/images/logo.svg';
-
 function App() {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    setIsActive(!isActive);
-  };
-
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>
-        Click on the Vite and React logos to learn more
-      </h1>
-      <button
-        onClick={handleClick}
-        className={classNames(styles.button, {
-          [styles.button__active]: isActive
-        })}
-      >
-        Click to Me!
-      </button>
-      <img
-        src={logo}
-        alt="logo vite"
-        className={classNames(styles.logo, { [styles.logo__hidden]: isActive })}
-      ></img>
-    </div>
-  );
+  return <h1>Привет, мир!</h1>;
 }
 
 export default App;
--- src/shared/modals/Modal.tsx
+++
@@ -1,7 +0,0 @@
-import { ModalProps } from './Modal.types';
-
-const Modal: React.FC<ModalProps> = () => {
-  return <></>;
-};
-
-export default Modal;
