80b3c01c7593428fa80a731c759d2cd83535d331
andigredient <endigredient@gmail.com>
2025-03-04 00:06:16 +0300

    проверил работоспособность шрифтов, поднял линтер, убрал директорию scss

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+//import '../assets/global.scss';
+import './App.scss'
+
+
+function App() {
+  return (
+  <>
+    <h1>Привет, мир!</h1>
+    <p className="montserrat">Это шрифт Montserrat</p>
+    <p className="open-sans">Это шрифт Open-Sans</p>
+    <p className="drukTextWideCur">Это шрифт DrukTextWideCur</p>
+
+  </>
+
+
+
+);
+}
+
+export default App;
+
