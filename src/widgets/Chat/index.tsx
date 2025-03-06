2ec327c60de6a63f94149f22033728fc89d70da7
Svetlana Podgainova <spodgainova@gmail.com>
2025-03-06 20:08:13 +0300

    fix/fix styles for chat & perks components

--- src/widgets/Chat/index.tsx
+++ src/widgets/Chat/index.tsx
@@ -1,12 +1,11 @@
 import styles from './index.module.scss';
 
-export const Chat : React.FC = () => {
+export const Chat: React.FC = () => {
   return (
     <div className={styles.chat}>
       <h2 className={styles.chat__title}>
-        Умные алгоритмы подберут{' '}
-        <span className={styles.accent}>преподавателя</span> под цели и
-        стоимость
+        Умные алгоритмы подберут&nbsp;
+        <span className={styles.accent}>преподавателя</span> под&nbsp;цели и&nbsp;стоимость
       </h2>
       <div className={styles.chat__viewport}></div>
     </div>
