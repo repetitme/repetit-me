0f0619ec1da0527bb2a6829dafcbb05f8844a96b
Svetlana Podgainova <spodgainova@gmail.com>
2025-03-06 19:56:25 +0300

    fix/fix styles for chat & perks components

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,13 +1,11 @@
 import { Chat } from '../../widgets/Chat';
 import { Perks } from '../../widgets/Perks';
-import { WhyWe } from '../../widgets/WhyWe';
 
 const MainPage = () => {
   return (
     <>
       <Perks />
       <Chat />
-      <WhyWe />
     </>
   );
 };
