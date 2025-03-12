a9f3e5cf5ea089e5a875fb3a94a09a7c031d84c2
andigredient <endigredient@gmail.com>
2025-03-12 23:39:21 +0300

    feat: добавление нового компонента Recruiting (блок 'Мы найдем учеников для нас')

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss';
 import '../assets/global.scss';
 import Main from '../pages/Main';
+import Recruiting from '../widgets/Recruiting';
 
 function App() {
   return (
     <>
       <Main />
+      <Recruiting />
     </>
   );
 }
