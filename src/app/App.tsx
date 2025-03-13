d81b582c5508cb2663a67239d758e4035c03e3eb
andigredient <endigredient@gmail.com>
2025-03-13 19:40:25 +0300

    fix: переместил компонент из App в Pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import './App.scss';
 import '../assets/global.scss';
 import Main from '../pages/Main';
-import Recruiting from '../widgets/Recruiting';
 
 function App() {
   return (
     <>
       <Main />
-      <Recruiting />
     </>
   );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,16 @@
 import Advantages from '../../widgets/Advantages';
+import Recruiting from '../../widgets/Recruiting';
 
 const Main = () => {
-  return <Advantages />;
+  return (
+    <>
+      <Advantages />
+      <Recruiting />    
+    </>
+
+
+
+  )
 };
 
 export default Main;
