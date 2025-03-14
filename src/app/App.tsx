5546284478fe3e2c2a3967461075590acd88abd0
Aleksandra <a.gerasevv@gmail.com>
2025-03-14 12:03:42 +0300

    fix: исправила вложенность стилей, добавила cursor: default для кнопки в disabled

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
-import './App.scss';
 import '../../src/assets/styles/globals.scss';
 import '../../src/assets/styles/variables.scss';
 import Button from '../shared/button';
+import './App.scss';
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Button variant="red" text="Эта кнопка красная" />
       <Button variant="purple" text="Эта кнопка сиреневая" />
       <Button variant="white" text="Эта кнопка прозрачная" size="large" />
+
     </>
   );
 }
