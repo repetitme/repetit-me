7efad8720e65be5732bbc5cc6c3b03c22b08c3f5
Svetlana Podgainova <spodgainova@gmail.com>
2025-03-17 13:34:10 +0300

    fix/fix bag with <p>

--- src/shared/components/ListItem/index.tsx
+++ src/shared/components/ListItem/index.tsx
@@ -14,7 +14,7 @@ export const ListItem: React.FC<ListItemProps> = ({
       <div className={styles.number}>{number}</div>
       <div className={styles.content}>
         <h3 className={styles.content__title}>{title}</h3>
-        <p className={styles.content__txt}>{text}</p>
+        {text}
       </div>
       <img src={image} alt={alt} />
     </li>
