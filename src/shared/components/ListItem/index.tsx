c40dea914acb62db1914609cf3092fc1e2bbecbd
Svetlana Podgainova <spodgainova@gmail.com>
2025-03-17 13:55:00 +0300

    fix/styles

--- src/shared/components/ListItem/index.tsx
+++ src/shared/components/ListItem/index.tsx
@@ -14,7 +14,7 @@ export const ListItem: React.FC<ListItemProps> = ({
       <div className={styles.number}>{number}</div>
       <div className={styles.content}>
         <h3 className={styles.content__title}>{title}</h3>
-        {text}
+        <div className={styles.content__txt}>{text}</div>
       </div>
       <img src={image} alt={alt} />
     </li>
