cd7fa088080883a3f5af9573785c9357a13ed2be
Svetlana Podgainova <spodgainova@gmail.com>
2025-03-20 22:43:38 +0300

    fix/resolve conflicts

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
 import { Chat } from '../../widgets/Chat';
-=======
 import Footer from '../../widgets/Footer';
 import { Perks } from '../../widgets/Perks';
->>>>>>> ed1c0aecc817c06cdfb821eb45111ce55e471541
 import QuestionList from '../../widgets/QuestionList';
 
 const MainPage = () => {
   return (
     <>
-<<<<<<< HEAD
+      <Perks />
       <Chat />
       <QuestionList />
-=======
-      <Perks />
       <QuestionList />
       <Footer />
->>>>>>> ed1c0aecc817c06cdfb821eb45111ce55e471541
     </>
   );
 };
