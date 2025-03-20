d22ee5d0d9fb2536dd031e9971293358b5617aa0
Svetlana Podgainova <spodgainova@gmail.com>
2025-03-20 22:55:03 +0300

    fix/delete return

--- src/widgets/WhyWe/index.tsx
+++ src/widgets/WhyWe/index.tsx
@@ -2,17 +2,15 @@ import { ListItem } from '../../shared/components/ListItem';
 import { listItems } from './data';
 import styles from './index.module.scss';
 
-export const WhyWe: React.FC = () => {
-  return (
-    <section className={styles.container}>
-      <h2 className={styles.title}>
-        Почему <span className={styles.accent}>мы</span>?
-      </h2>
-      <ul className={styles.list}>
-        {listItems.map((item) => (
-          <ListItem key={item.number} {...item} />
-        ))}
-      </ul>
-    </section>
-  );
-};
+export const WhyWe: React.FC = () => (
+  <section className={styles.container}>
+    <h2 className={styles.title}>
+      Почему <span className={styles.accent}>мы</span>?
+    </h2>
+    <ul className={styles.list}>
+      {listItems.map((item) => (
+        <ListItem key={item.number} {...item} />
+      ))}
+    </ul>
+  </section>
+);
