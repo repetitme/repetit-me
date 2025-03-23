692a0de15ff7174cd814c9ca29b0dee3a9247b32
andigredient <endigredient@gmail.com>
2025-03-23 23:25:17 +0300

    fix: устранил замечания

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -3,12 +3,14 @@ import { Chat } from '../../widgets/Chat';
 import Footer from '../../widgets/Footer';
 import { Perks } from '../../widgets/Perks';
 import QuestionList from '../../widgets/QuestionList';
+import TutorFormBlock from '../../widgets/TutorFormBlock';
 
 const MainPage = () => {
   return (
     <>
       <Perks />
       <WhyWe />
+      <TutorFormBlock />
       <Chat />
       <QuestionList />
       <Footer />
