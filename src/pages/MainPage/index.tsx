4b8a78ad5ee94079af7f472f095c5127edc6a356
andigredient <endigredient@gmail.com>
2025-04-03 01:18:16 +0300

    fix: подправил mainPage

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,8 +1,3 @@
-import { WhyWe } from '../../widgets/WhyWe';
-import { Chat } from '../../widgets/Chat';
-import Footer from '../../widgets/Footer';
-import { Perks } from '../../widgets/Perks';
-import QuestionList from '../../widgets/QuestionList';
 import QuickSelection from '../../widgets/QuickSelection';
 import MainBlock from '../../widgets/MainBlock';
 import { WhyWe } from '../../widgets/WhyWe';
