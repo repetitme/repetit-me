7f5585eecb5a78dc903fc2117108034eb4015056
Svetlana Podgainova <spodgainova@gmail.com>
2025-04-06 13:51:38 +0300

    delete block from App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import '../../src/assets/styles/globals.scss';
 import '../../src/assets/styles/variables.scss';
-import { TelegramBlock } from '../shared/components/TelegramBlock';
 import './App.scss';
 import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <>
-    <TelegramBlock />
       <AppRouter />
     </>
   );
