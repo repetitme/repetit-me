328cbb12365e8688d4a7b300be05f47db84b2994
anton <zanarovanthon@yandex.com>
2025-04-07 13:21:32 +1000

    fix: исправление ошибки сборки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import '../assets/index.scss';
 function App() {
   return (
     <div className="App">
-      <AuthModalWithForm isOpen={true} />
+      <AuthModalWithForm closeModal={() => console.log(1)} />
     </div>
   );
 }
