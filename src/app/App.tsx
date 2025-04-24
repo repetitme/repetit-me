ac9fd843329f87c8f7fbe86a8c82409470b791c4
andigredient <endigredient@gmail.com>
2025-04-25 01:50:47 +0300

    fix: app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 
+import AuthForm from '../features/auth/form';
+import ReviewList from '../shared/components/SortByDate';
 import AppRouter from './router/AppRouter';
 import Header from '../widgets/Header';
 import Footer from '../widgets/Footer';
 
 import MainPage from '../pages/MainPage';
 
-import '../../src/assets/styles/globals.scss';
-import '../../src/assets/styles/variables.scss';
+import '../assets/index.scss';
 import style from './index.module.scss';
 
 function App() {
@@ -16,24 +16,12 @@ function App() {
   return (
     <>
       <div className={style.app}>
+        <AuthForm login={false} />
+        <ReviewList/>
         <AppRouter />
         <MainPage />
       </div>
     </>
   );
-=======
-import AuthForm from '../features/auth/form';
-
-import '../assets/index.scss';
-import ReviewList from '../shared/components/SortByDate';
-
-function App() {
-  return (<>
-    <ReviewList/>
-    <div className="App">
-      <AuthForm login={false} />
-    </div>
- </> );
->>>>>>> 060131c9a5e49fa7324a2435cd750760b734c0cb
 }
 export default App;
