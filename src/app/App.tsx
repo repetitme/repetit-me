6f78fdefc2aad1e8ec8188040aaa224481cba0b7
andigredient <endigredient@gmail.com>
2025-04-25 22:13:02 +0300

    fix: поднял линтер в компоненте App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,4 @@
-import { useState } from 'react';
-
-import AuthForm from '../features/auth/form';
 import MainPage from '../pages/MainPage';
-import ReviewList from '../shared/components/SortByDate';
-import Footer from '../widgets/Footer';
-import Header from '../widgets/Header';
 import AppRouter from './router/AppRouter';
 
 import '../assets/index.scss';
