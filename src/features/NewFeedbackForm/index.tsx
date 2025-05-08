de3234f86955e9fc90f353fe7535bf3e674b0e9a
Aleksandra <a.gerasevv@gmail.com>
2025-05-08 13:42:18 +0300

    add max min length

--- src/features/NewFeedbackForm/index.tsx
+++ src/features/NewFeedbackForm/index.tsx
@@ -7,6 +7,9 @@ import styles from '../NewFeedbackForm/index.module.scss';
 
 import { NewFeedbackFormProps } from './type';
 
+const MIN_LENGTH = 5;
+const MAX_LENGTH = 500;
+
 const NewFeedbackForm: React.FC<NewFeedbackFormProps> = ({ toggleVisible }) => {
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState('');
@@ -20,14 +23,14 @@ const NewFeedbackForm: React.FC<NewFeedbackFormProps> = ({ toggleVisible }) => {
     const newContent = e.target.value;
     setContent(newContent);
 
-    if (newContent.trim().length < 5) {
-      setError(`Отзыв должен содержать минимум 5 символов.`);
-      return;
-    }
-
-    if (newContent.trim().length > 20) {
-      setError(`Отзыв не должен превышать 500 символов.`);
-      return;
+    const trimmedLength = newContent.trim().length;
+    
+    if (trimmedLength >= MAX_LENGTH) {
+      setError(`Отзыв не должен превышать ${MAX_LENGTH} символов.`);
+    } else if (trimmedLength < MIN_LENGTH) {
+      setError(`Отзыв должен содержать минимум ${MIN_LENGTH} символов.`);
+    } else {
+      setError('');
     }
   };
 
@@ -40,7 +43,7 @@ const NewFeedbackForm: React.FC<NewFeedbackFormProps> = ({ toggleVisible }) => {
   };
 
   const isButtonDisabled =
-    rating === 0 || content.trim().length < 5 || content.trim().length > 500;
+  rating === 0 || content.trim().length < MIN_LENGTH || content.trim().length > MAX_LENGTH;
 
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
@@ -53,9 +56,9 @@ const NewFeedbackForm: React.FC<NewFeedbackFormProps> = ({ toggleVisible }) => {
         <h4>Ваш отзыв</h4>
         <textarea
           value={content}
-          onInput={handleContentChange}
+          onChange={handleContentChange}
           placeholder="Общее впечатление; что понравилось в уроках; какие результаты"
-          maxLength={20}
+          maxLength={MAX_LENGTH}
           required
         />
       </label>
