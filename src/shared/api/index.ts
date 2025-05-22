e89825e7d2ca714bc8473c19b8f2356e7da9abd7
andigredient <endigredient@gmail.com>
2025-05-22 15:40:07 +0300

    fix:исправил обработчики, добавил экспорт

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,22 +1,22 @@
 const URL = process.env.REPETIT_ME_URL;
 
-async function httpJson<T>(path: string): Promise<T> {
+export async function httpJson<T>(path: string): Promise<T> {
 
   const response = await fetch(`${URL}/${path}`, {
     headers: {
       'Content-Type': 'application/json',
     }    
   })
-  .then(response => {
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
     if (!response.ok) {
-      return response.json()
-        .catch(() => response.text())
-    }})
-    
-  if (!response.ok) {
-    throw new Error(`Error, status: ${response.status}`);
-  }
-  return response.json() as Promise<T>;
-
-  
+      throw new Error(`Error, status: ${response.status}`);
+    }
+  })
+  .catch((err) => {
+    console.log("Запрос не выполнен ",err)
+  })  
+  return response.json() as Promise<T>;  
 }
