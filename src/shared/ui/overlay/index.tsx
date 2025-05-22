2d4baafc4ea569a4172f6c11ef194c8fde2b0126
ozalexander <156056519+ozalexander@users.noreply.github.com>
2025-05-22 12:50:10 +0300

    df

--- src/shared/ui/overlay/index.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './index.module.scss';
-
-import { ModalOverlayProps } from './type';
-
-export const ModalOverlay = ({ onClose }: ModalOverlayProps) => (
-  <div className={styles.overlay} onClick={onClose} />
-);
