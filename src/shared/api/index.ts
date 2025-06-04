d044e6c175784f3602bfd6ff54877d29c8a7c02e
andigredient <endigredient@gmail.com>
2025-06-04 22:52:33 +0300

    fix: прогнал линтером

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,22 +1,21 @@
 const URL = process.env.REPETIT_ME_URL;
 
 export async function httpJson<T>(path: string): Promise<T> {
-
   const response = await fetch(`${URL}/${path}`, {
     headers: {
-      'Content-Type': 'application/json',
-    }    
-  })
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    if (!response.ok) {
-      throw new Error(`Error, status: ${response.status}`);
+      'Content-Type': 'application/json'
     }
   })
-  .catch((err) => {
-    throw new Error(`Запрос не выполнен ,${err}`);
-  })  
-  return response; 
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      if (!response.ok) {
+        throw new Error(`Error, status: ${response.status}`);
+      }
+    })
+    .catch((err) => {
+      throw new Error(`Запрос не выполнен ,${err}`);
+    });
+  return response;
 }
