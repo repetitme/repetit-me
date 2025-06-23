844a919313afaadf07e399cf983aebb9318afcc1
Maxim Beketov <cleanerkerri@gmail.com>
2025-06-23 19:42:55 +0500

    feat/initial task

---
+++ src/features/TutorQuestionnaire/videoGreetingBlock/ui/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import styles from './index.module.scss';
+
+const VideoGreetingBlock: React.FC = () => {
+  return <h3>Видео</h3>;
+};
+
+export default VideoGreetingBlock;
