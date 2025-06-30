df1c5ead3575be090aceef17442c270ef6305938
ozalexander <156056519+ozalexander@users.noreply.github.com>
2025-06-30 08:10:43 +0300

    fix: dep

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import Footer from '../widgets/Footer';
 import Header from '../widgets/Header';
-import StatCards from '../widgets/StatisticCards';
 import AppRouter from './router/AppRouter';
 
 import '../assets/index.scss';
@@ -9,7 +8,6 @@ import styles from './index.module.scss';
 function App() {
   return (
     <div className={styles.app}>
-      <StatCards />
       <Header auth={'student'} />
       <AppRouter />
       <Footer role="student" />
