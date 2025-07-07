2808bd4e326ff1221334069266b78b70115eaa4a
Aleksandra <a.gerasevv@gmail.com>
2025-07-07 20:33:52 +0300

    refactor: сделала Header единым компонентом

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,44 +1,57 @@
 import { useEffect, useState } from 'react';
 
+import cn from 'classnames';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
+import avatar from '../../assets/images/avatarHeader.png';
 import logo from '../../assets/images/logo.svg';
 import icon from '../../assets/images/telegram_icon.svg';
 import Button from '../../shared/ui/button';
-import StudentHeader from './StudentHeader';
-import TutorHeader from './TutorHeader';
+import getHeaderConfig from './getHeaderConfig';
 
 import styles from './index.module.scss';
 
 import { HeaderProps, TUserRole } from './types';
 
 const Header = ({ auth }: HeaderProps) => {
-  const [authHeader, setAuthHeader] = useState<TUserRole>(
-    auth ? auth : 'unauth'
-  );
-
+  const [authHeader, setAuthHeader] = useState<TUserRole>(auth || 'unauth');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    setAuthHeader(auth);
-  }, [auth]);
 
   const handleLogout = () => {
     setAuthHeader('unauth');
     navigate('/');
   };
 
+  const { navItems, dropDownItems } = getHeaderConfig({
+    role: authHeader,
+    onLogout: handleLogout,
+    navigate,
+    location
+  });
+
+  useEffect(() => {
+    setAuthHeader(auth);
+  }, [auth]);
+
   return (
     <header className={styles.header}>
       <Link to="/" className={styles.header__logo} aria-label="На главную">
         <img src={logo} alt="Логотип Repetit Me" />
       </Link>
-      {authHeader === 'unauth' && (
+
+      {authHeader === 'unauth' ? (
         <nav className={styles.header__nav}>
-          <Link to="/tutors" className={styles.header__nav__link}>
-            Репетиторы
-          </Link>
+          {navItems.unauth.map((item) => (
+            <Link
+              key={item.text}
+              to={item.path}
+              className={styles.header__nav__link}
+            >
+              {item.text}
+            </Link>
+          ))}
           <Link
             to="/register"
             className={styles.header__nav__link}
@@ -46,7 +59,6 @@ const Header = ({ auth }: HeaderProps) => {
           >
             Войти
           </Link>
-
           <Button
             variant="social"
             size="large"
@@ -55,12 +67,57 @@ const Header = ({ auth }: HeaderProps) => {
             className={styles.telegram}
           />
         </nav>
-      )}
-      {authHeader === 'student' && (
-        <StudentHeader role="student" onLogout={handleLogout} />
-      )}
-      {authHeader === 'tutor' && (
-        <TutorHeader role="tutor" onLogout={handleLogout} />
+      ) : (
+        <>
+          <nav className={styles.header__nav}>
+            {navItems[authHeader].map((item) => (
+              <Link
+                key={item.text}
+                to={item.path}
+                className={styles.header__nav__link}
+              >
+                {item.text}
+              </Link>
+            ))}
+          </nav>
+
+          <div
+            className={styles.header__avatar}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <img src={avatar} alt="Аватар" />
+            <div className={styles.header__avatar__image} />
+
+            <div
+              className={cn(styles.header__submenu, {
+                [styles['header__submenu--active']]: isMenuOpen
+              })}
+            >
+              {dropDownItems.map((item) => (
+                <button
+                  key={item.text}
+                  className={styles.header__submenu__list__item}
+                  onClick={item.onClick}
+                >
+                  {item.icon && (
+                    <img
+                      src={item.icon}
+                      alt=""
+                      className={styles.header__submenu__list__icon}
+                    />
+                  )}
+                  {item.text}
+                </button>
+              ))}
+            </div>
+
+            <button
+              className={cn(styles.header__avatar__chevron, {
+                [styles['header__avatar__chevron--active']]: isMenuOpen
+              })}
+            />
+          </div>
+        </>
       )}
     </header>
   );
--- src/widgets/Header/types.ts
+++ src/widgets/Header/types.ts
@@ -1,23 +1,30 @@
+import { Location, NavigateFunction } from 'react-router';
+
 export type TUserRole = 'unauth' | 'student' | 'tutor';
 
 export interface HeaderProps {
   auth: TUserRole;
 }
 
-export interface CommonHeaderProps {
-  role: Exclude<TUserRole, 'unauth'>;
+export interface HeaderConfigProps {
+  role: TUserRole;
   onLogout: () => void;
+  navigate: NavigateFunction;
+  location: Location;
 }
 
 export interface NavItem {
   text: string;
-  path?: string;
+  path: string;
   icon?: string;
   onClick?: () => void;
+  withLocationState?: boolean;
 }
 
-export interface AuthHeaderProps {
-  navItems: NavItem[];
-  avatarMenuItems: NavItem[];
-  avatarImage?: string;
+export interface DropDownItem {
+  text: string;
+  icon?: string;
+  onClick: () => void;
 }
+
+
