b6284eac4acaad8c77d89c443249a038b7426f98
андрей власов <dyushavlasov@gmail.com>
2025-08-03 01:34:36 +0300

    fix: изменена директория AboutService

--- src/features/TutorCabinet/ui/AboutService/components/popupContainer/index.tsx
+++ src/features/TutorCabinet/ui/AboutService/components/popupContainer/index.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react';
 
 import closeIcon from '../../../../assets/icons/close.svg';
-import useClickOutside from '../../../../shared/hooks/useClickOutside';
-import Button from '../../../../shared/ui/button';
-import ModalOverlay from '../../../../shared/ui/overlay';
+import useClickOutside from '../../../../../../shared/hooks/useClickOutside';
+import Button from '../../../../../../shared/ui/button';
+import ModalOverlay from '../../../../../../shared/ui/overlay';
 
 import styles from './index.module.scss';
 
