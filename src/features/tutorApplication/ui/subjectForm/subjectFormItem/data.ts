5d752ed2e934e4fa56832bed275e73a623f4f074
Aleksandra Geraseva <156932672+SashaMNG@users.noreply.github.com>
2025-08-26 14:40:14 +0300

    bag268:  добавила предметы в анкету

--- src/features/tutorApplication/ui/subjectForm/subjectFormItem/data.ts
+++ src/features/tutorApplication/ui/subjectForm/subjectFormItem/data.ts
@@ -9,7 +9,23 @@ export const disciplines = [
   { value: 'environmental_studies', label: 'Окружающий мир' },
   { value: 'geography', label: 'География' },
   { value: 'history', label: 'История' },
-  { value: 'informatics', label: 'Информатика' }
+  { value: 'informatics', label: 'Информатика' },
+  { value: 'english', label: 'Английский язык' },
+  { value: 'german', label: 'Немецкий язык' },
+  { value: 'chinese', label: 'Китайский язык' },
+  { value: 'french', label: 'Французский язык' },
+  { value: 'italian', label: 'Итальянский язык' },
+  { value: 'spanish', label: 'Испанский язык' },
+  { value: 'japanese', label: 'Японский язык' },
+  { value: 'turkish', label: 'Турецкий язык' },
+  { value: 'finnish', label: 'Финский язык' },
+  { value: 'estonian', label: 'Эстонский язык' },
+  { value: 'children_speech_therapist', label: 'Детский логопед' },
+  { value: 'piano', label: 'Фортепиано' },
+  { value: 'guitar', label: 'Гитара' },
+  { value: 'violin', label: 'Скрипка' },
+  { value: 'solfeggio', label: 'Сольфеджио' },
+  { value: 'speech_training', label: 'Постановка речи' }
 ];
 
 export const status = [
