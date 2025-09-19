55b25e2cdc8fe4814b6b8c8a0100c2d335f0070b
андрей власов <dyushavlasov@gmail.com>
2025-09-20 02:21:13 +0300

    fix: исправление багов, выявленных на тестировке

--- src/features/TutorCabinet/ui/AboutService/components/popupContainer/index.tsx
+++ src/features/TutorCabinet/ui/AboutService/components/popupContainer/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-import closeIcon from '../../../../../../assets/icons/closeIcon.svg';
+import closeIcon from '../../../../../../assets/icons/close.svg';
 import useClickOutside from '../../../../../../shared/hooks/useClickOutside';
 import Button from '../../../../../../shared/ui/button';
 import ModalOverlay from '../../../../../../shared/ui/overlay';
