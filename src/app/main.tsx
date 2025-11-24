75d4d3b97aeb3a95f99fa92e1e16bdba8efcadcd
ozalexander <156056519+ozalexander@users.noreply.github.com>
2025-11-24 12:44:43 +0300

    fix: final

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -6,6 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import QueryProvider from '../app/providers/reactQuery/QueryProvider.tsx';
 import App from './App.tsx';
 
+window.addEventListener('pageshow', (event) => {
+  if (event.persisted) {
+    document.body.style.display = 'none';
+    requestAnimationFrame(() => {
+      document.body.style.display = '';
+    });
+  }
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryProvider>
